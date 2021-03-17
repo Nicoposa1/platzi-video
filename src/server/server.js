2b4bfd772830bc7cddebd7d84fdf1688a54ec5e4
Nicolas Posa <nicoposa57@gmail.com>
2021-03-17 17:40:19 -0300

    corriendo react con Express

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
+/* eslint-disable global-require */
 import express from 'express';
 import dotenv from 'dotenv';
+import webpack from 'webpack';
 
 dotenv.config();
 
@@ -8,11 +10,33 @@ const app = express();
 
 if (ENV === 'development') {
   console.log('Development config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const { publicPath } = webpackConfig.output;
+  const serverConfig = { serverSideRender: true, publicPath };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+
 }
 
 app.get('*', (req, res) => {
   console.log('Hola');
-  res.send({ hello: 'express' });
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <Link href="assets/app.css" rel="stylesheet" type="text/css" />
+        <title>Platzi Video</title>
+      </head>
+      <body>
+        <div id="app"></div>
+        <script src="assets/app.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, (err) => {
